492e1d13f6c2511345018154d1c467898addf0d8
m <lmiguelvargasf@gmail.com>
2015-11-08 22:10:31 -0500

    Complete program to look for a phone number and an e-mail in text copied to the clipboard.

---
+++ scripts/phone_number_and_email_extractor.py
@@ -0,0 +1,46 @@
+#! python3.4
+# phone_number_and_email_extractor.py - Finds phone number and email addresses on the clipboard.
+
+import pyperclip, re
+
+
+def main():
+    phone_regex = re.compile(r"""(
+        (\d{3}|\(\d{3}\))?              # area code
+        (\s|-|\.)?                      # separator
+        (\d{3})                         # first 3 digits
+        (\s|-|\.)                       # separator
+        (\d{4})                         # last 4 digits
+        (\s*(ext|x|ext.)\s*(\d{2,5}))?  # extension
+        )""", re.VERBOSE)
+
+
+    email_regex = re.compile(r"""(
+        [a-zA-Z0-9._+-]+                # username
+        @                               # @ symbol
+        [a-zA-Z0-9.-]+                  # domain name
+        (\.[a-zA-Z]{2,4})               # dot-something
+        )""", re.VERBOSE)
+
+    text = str(pyperclip.paste())
+    matches = []
+
+    for groups in phone_regex.findall(text):
+        phone_num = "-".join([groups[1], groups[3], groups[5]])
+        if groups[8] != "":
+            phone_num += " x" + groups[8]
+        matches.append(phone_num)
+
+    for groups in email_regex.findall(text):
+        matches.append(groups[0])
+
+    if len(matches) > 0:
+        pyperclip.copy("\n".join(matches))
+        print("Copied to clipboard:")
+        print("\n".join(matches))
+    else:
+        print("No phone number or e-mail addresses found.")
+
+
+if __name__ == "__main__":
+    main()
