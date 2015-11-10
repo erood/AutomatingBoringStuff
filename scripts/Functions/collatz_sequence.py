fce3bdb5f5eb3f1ed2213749ceae29569c507a9c
m <lmiguelvargasf@gmail.com>
2015-11-09 20:53:06 -0500

    Rename files, and refacctor the collatz_sequence.py.

--- scripts/Functions/collatz_sequence.py
+++ scripts/Functions/collatz_sequence.py
@@ -1,26 +1,24 @@
 #! python3
-__author__ = 'm'
-
-"""
-Write a function named collatz() that has one parameter named number. If
-number is even, then collatz() should print number // 2 and return this value.
-If number is odd, then collatz() should print and return 3 * number + 1.
-Then write a program that lets the user type in an integer and that keeps
-calling collatz() on that number until the function returns the value 1.
-"""
+# collatz_sequence.py - generates a collatz sequence
 
 import sys
+
+__author__ = 'm'
+
 
 def generate_next_collatz_number(number):
     if number % 2 == 0:
         return number // 2
+
     return 3 * number + 1
 
 
 def generate_collatz_sequence(number):
     sequence = [generate_next_collatz_number(number)]
+
     while sequence[-1] != 1:
         sequence.append(generate_next_collatz_number(sequence[-1]))
+
     return sequence
 
 
