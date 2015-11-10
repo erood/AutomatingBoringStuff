83e09ba6e07379cd7720bcb6e359e51a720d14f2
m <lmiguelvargasf@gmail.com>
2015-11-09 21:10:47 -0500

    Rename and refactor fantasy_game_inventory.py.

--- scripts/Dictionaries/fantasy_game_inventory.py
+++ scripts/Dictionaries/fantasy_game_inventory.py
@@ -1,21 +1,15 @@
 #! python3
+# fantasy_game_inventory.py -  takes a dictionary and prints its contents in value key format
 __author__ = 'm'
-
-"""
-You are creating a fantasy video game. The data structure to model the
-player's inventory will be a dictionary where the keys are string values
-describing the item in the inventory and the value is an integer value detailing
-how many of that item the player has. For example, the dictionary value
-{'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12} means the
-player has 1 rope, 6 torches, 42 gold coins, and so on
-"""
 
 
 def display_inventory(inventory):
     total_number_of_items = 0
+
     for item, quantity in inventory.items():
         print(quantity, item)
         total_number_of_items += quantity
+
     print("Total number of items: " + str(total_number_of_items))
 
 
