d946b786f337f8d1572cb069cc6d30327614ca1f
m <lmiguelvargasf@gmail.com>
2015-11-23 22:30:08 -0500

    Modify .gitignore and implement function to generate random quizzes.

--- RWFiles/random_quiz_generator.py
+++ RWFiles/random_quiz_generator.py
@@ -2,8 +2,11 @@
 # random_quiz_generator.py - creates quizzes with questions and answers in random order, along with the answer key.
 
 import random
+import sys
 
-capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix', 'Arkansas': 'Little Rock',
+NUMBER_OF_QUIZZES = 35
+
+CAPITALS = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix', 'Arkansas': 'Little Rock',
             'California': 'Sacramento', 'Colorado': 'Denver', 'Connecticut': 'Hartford', 'Delaware': 'Dover',
             'Florida': 'Tallahassee', 'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise',
             'Illinois': 'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Moines', 'Kansas': 'Topeka',
@@ -19,11 +22,60 @@ capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix', '
 
 
 def main():
-    pass
-    # TODO: Create the quiz and answer key files.
-    # TODO: Write out the header for the quiz.
-    # TODO: Shuffle the order of the states.
-    # TODO: Loop through all 50 states, making a question for each.
+    for quiz_number in range(NUMBER_OF_QUIZZES):
+        generate_quiz(quiz_number)
+
+
+def generate_quiz(quiz_number):
+    number = quiz_number + 1
+    quiz = create_quiz("capitalquiz%s.txt", number)
+    answer_key = create_answer_key("capitalquiz_answers%s.txt", number)
+    write_header(quiz, number)
+    states = list(CAPITALS.keys())
+    random.shuffle(states)
+
+    for question_number in range(len(CAPITALS)):
+        correct_answer, answer_options = get_answer_options(states, question_number, 3)
+        random.shuffle(answer_options)
+        write_question(answer_options, question_number, quiz, states)
+        write_answer_key(answer_key, question_number, answer_options, correct_answer)
+
+    quiz.close()
+    answer_key.close()
+
+
+def create_quiz(name, number):
+    return open(name % number, "w")
+
+
+def create_answer_key(name, number):
+    return open(name % number, "w")
+
+
+def write_header(file, number):
+    file.write("Name:\n\nDate:\n\nPeriod:\n\n")
+    file.write((' ' * 20) + "State Capitals Quiz (Form %s)" % number)
+    file.write("\n\n")
+
+
+def get_answer_options(states, question_number, options):
+    correct_answer = CAPITALS[states[question_number]]
+    wrong_answers = list(CAPITALS.values())
+    del wrong_answers[wrong_answers.index(correct_answer)]
+    wrong_answers = random.sample(wrong_answers, options)
+
+    return correct_answer, wrong_answers + [correct_answer]
+
+
+def write_question(answer_options, question_number, quiz, states):
+    quiz.write("%s. What is the capital of %s?\n" % (question_number + 1, states[question_number]))
+    for i in range(len(answer_options)):
+        quiz.write("\t%s. %s\n" % ("ABCD"[i], answer_options[i]))
+    quiz.write("\n")
+
+
+def write_answer_key(file, question_number, answer_options, correct_answer):
+    file.write("%s. %s\n" % (question_number + 1, "ABCD"[answer_options.index(correct_answer)]))
 
 
 if __name__ == "__main__":
